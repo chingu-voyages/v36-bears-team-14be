93e49059af29f298ab9feed37735d4f756506f3f
MarilynPapadopoulos <marilyn.volk@gmail.com>
2022-01-13 15:58:47 -0500

    added timestamps

--- src/models/recipe/recipe.schema.ts
+++ src/models/recipe/recipe.schema.ts
@@ -3,28 +3,33 @@ import mongoose from "mongoose";
 import { IRecipeDocument, IRecipeModel } from "./recipe.types";
 
 //Recipe Schema
-const RecipeSchema: Schema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const RecipeSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    desription: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      key: String,
+      ref: "user",
+      value: Date,
+    },
+    postedBy: {
+      type: String,
+      ref: "user",
+    },
+    images: {
+      type: [{ url: String }],
+    },
   },
-  desription: {
-    type: String,
-    required: true,
-  },
-  likes: {
-    key: String,
-    ref: "user",
-    value: Date,
-  },
-  postedBy: {
-    type: String,
-    ref: "user",
-  },
-  images: {
-    type: [{ url: String }],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default RecipeSchema;
 // export const RecipeModel = model<IRecipeDocument, IRecipeModel>('recipes', RecipeSchema, 'recipes')
--- src/models/recipe/recipe.types.ts
+++ src/models/recipe/recipe.types.ts
@@ -3,12 +3,12 @@ export interface IRecipe {
   _id: string;
   name: string;
   description: string;
-  likes: { [keyof: string]: Date }
+  likes: { [keyof: string]: Date };
   postedBy: string;
   images: Array<{ url: string }>;
-createdAt: Date;
-updatedAt: Date
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-export interface IRecipeDocument extends IRecipe, Document {}
+export interface IRecipeDocument extends Document<IRecipe> {}
 export interface IRecipeModel extends Model<IRecipeModel> {}
