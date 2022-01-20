0de1b846b87cd28fdab3f85dea0bddc59f15f45b
David Eastmond <davebuck1981@gmail.com>
2022-01-19 21:58:36 -0500

    added get routes and route controllers

--- src/routes/controllers/user/user.get.controller.ts
+++ src/routes/controllers/user/user.get.controller.ts
@@ -1,13 +1,51 @@
 import { Request, Response, NextFunction } from "express";
-
-export const getUserById = async (req: Request, res: Response) => {};
+import { UserModel } from "../../../models/user/user.schema";
+interface IRequestUser extends Request {
+  user: {
+    id: string;
+  };
+}
+export const getUserById = async (req: IRequestUser, res: Response) => {
+  const { id } = req.params;
+  try {
+    const secureUser = await UserModel.getUserByIdSecure({ id });
+    if (secureUser) {
+      return res.status(200).send(secureUser);
+    } else {
+      return res.status(404).send({ error: `User with id ${id} not found` });
+    }
+  } catch (exception) {
+    return res.status(500).send({ error: exception.message });
+  }
+};
 
 export const getUserByIdMe = async (
-  req: Request,
-  res: Express.Response,
+  req: IRequestUser,
+  res: Response,
   next: NextFunction
 ) => {
   if (req.params.id !== "me") next();
   try {
-  } catch (exception) {}
+    const { id } = req.user;
+    const secureUser = await UserModel.getUserByIdSecure({ id });
+    if (secureUser) {
+      return res.status(200).send(secureUser);
+    } else {
+      return res.status(404).send({ error: `Cannot find session user by id` });
+    }
+  } catch (exception) {
+    return res.status(500).send({ error: exception.message });
+  }
+};
+
+export const getAllUsersSecureMiddleWare = async (
+  req: IRequestUser,
+  res: Response
+) => {
+  try {
+    const secureUsers = await UserModel.getAllUsersSecure();
+    return res.status(200).send(secureUsers);
+  } catch (exception) {
+    return res.status(500).send({ error: exception.message });
+  }
 };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,14 @@
 import * as express from "express";
 import { protectedRoute } from "../middleware/protected-route";
 import {
+  getAllUsersSecureMiddleWare,
   getUserById,
   getUserByIdMe,
 } from "./controllers/user/user.get.controller";
 import { getParamIdValidator, validate } from "./validators";
 const router = express.Router();
 
+router.get("/", protectedRoute, getAllUsersSecureMiddleWare);
 router.get(
   "/:id",
   protectedRoute,
