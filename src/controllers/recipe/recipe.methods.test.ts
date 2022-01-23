870db4cb27779326229a46da43db695c2edf4c19
David Eastmond <davebuck1981@gmail.com>
2022-01-23 06:39:47 -0500

    add likes toggling tests

--- src/controllers/recipe/recipe.methods.test.ts
+++ src/controllers/recipe/recipe.methods.test.ts
@@ -1,5 +1,7 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import { createDummyRecipes } from "../utils/create-dummy-recipes";
+import { createTestUsers } from "../utils/create-dummy-users";
 
 let mongoServer: any;
 
@@ -22,4 +24,29 @@ beforeEach(async () => {
 afterEach(async () => {
   await mongoose.disconnect();
   await mongoServer.stop();
+});
+
+describe("recipe toggle likes tests", () => {
+  test("toggles likes properly", async () => {
+    const testUsers = await createTestUsers({
+      count: 3,
+      plainTextPassword: "password",
+    });
+    const testRecipe = await createDummyRecipes({
+      count: 1,
+      createdByUserId: testUsers[0]._id.toString(),
+    });
+
+    await testRecipe[0].toggleLike(testUsers[1]._id.toString());
+    await testRecipe[0].toggleLike(testUsers[2]._id.toString());
+
+    expect(testRecipe[0].likes[`${testUsers[1]._id.toString()}`]).toBeDefined();
+
+    await testRecipe[0].toggleLike(testUsers[1]._id.toString());
+    expect(
+      testRecipe[0].likes[`${testUsers[1]._id.toString()}`]
+    ).toBeUndefined();
+    expect(testRecipe[0].likes[`${testUsers[2]._id.toString()}`]).toBeDefined();
+    expect(Object.keys(testRecipe[0].likes).length).toBe(1);
+  });
 });
